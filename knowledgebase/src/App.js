089821a0d5ba169aa056d16d9d149f94e60b8a85
Aathi Manikandan <129000936+oCt-raiN@users.noreply.github.com>
2023-05-12 17:38:52 +0000

    jsx

--- knowledgebase/src/App.js
+++ knowledgebase/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {Button } from 'react-bootstrap';
 
 function App() {
   return (
@@ -9,13 +11,22 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button href="http://celaicindia.in/"> This is a </Button>
+        <a
+          className='as'
+          href='celiacindia.in'
+          target='_blank'
+          rel='noopener noreferrer'
+          >
+            celiac database
+            </a>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        My project
         </a>
       </header>
     </div>
