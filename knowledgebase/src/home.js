c73246f26173e1770be2984b63ba16cf2cc62df6
Aathi Manikandan <129000936+oCt-raiN@users.noreply.github.com>
2023-06-11 16:50:27 +0000

    restart-project

---
+++ knowledgebase/src/home.js
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import { Nav } from 'react-bootstrap';
+import './home.css';
+
+function Home() {
+    return(
+    <body>
+       <nav class="navbar navbar-expand-lg bg-dark navbar-light fixed-top ">
+        <div className="container">
+            <a href="#" className=""></a>
+        </div>
+       </nav>
+    </body>
+    );
+}
+
+export default Home;
